93268a0c5673c4d59fc7a889e14858285bb6a181
Ronald Garcia <ronaldgm@gmail.com>
2025-03-08 20:08:53 -0500

    Add variables for 2 numbers and operator

--- calculator.js
+++ calculator.js
@@ -4,3 +4,7 @@ const calcAdd = (num1, num2) => num1 + num2;
 const calcSubtract = (num1, num2) => num1 - num2;
 const calcMultiply = (num1, num2) => num1 * num2;
 const calcDivide = (num1, num2) => num1 / num2;
+
+let numInput1 = 0;
+let numInput2 = 0;
+let operator = '';
