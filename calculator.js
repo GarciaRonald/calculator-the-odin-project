b7903d528b115b8c6dac71f9e5d6d725a116c331
Ronald Garcia <ronaldgm@gmail.com>
2025-03-08 19:51:33 -0500

    Add basic add, subtract, multiply and divide functions

---
+++ calculator.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const calcAdd = (num1, num2) => num1 + num2;
+const calcSubtract = (num1, num2) => num1 - num2;
+const calcMultiply = (num1, num2) => num1 * num2;
+const calcDivide = (num1, num2) => num1 / num2;
