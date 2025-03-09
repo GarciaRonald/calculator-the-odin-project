4378f1ccee818a7d57296c02c66cb1ccd4380060
Ronald Garcia <ronaldgm@gmail.com>
2025-03-08 21:51:42 -0500

    Add operate() function that invokes basic math functions based on operator

--- calculator.js
+++ calculator.js
@@ -8,3 +8,15 @@ const calcDivide = (num1, num2) => num1 / num2;
 let numInput1 = 0;
 let numInput2 = 0;
 let operator = '';
+
+const operate = (num1, oper, num2) => {
+    if (oper === '+') {
+        return calcAdd(num1, num2);
+    } else if (oper === '-') {
+        return calcSubtract(num1, num2);
+    } else if (oper === '*') {
+        return calcMultiply(num1, num2);
+    } else if (oper === '/') {
+        return calcDivide(num1, num2);
+    }
+};
